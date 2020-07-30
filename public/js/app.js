afcd6b023b217a31bdeec5b1f8b5a87f9664308c
Wahyu Setya Permadi <wsetyapermadimobile@gmail.com>
2020-07-30 09:24:50 +0700

    add weather icon and weather description

--- public/js/app.js
+++ public/js/app.js
@@ -2,22 +2,30 @@ const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
+const messageThree = document.querySelector('#message-3');
+const image = document.querySelector('img');
 
 weatherForm.addEventListener('submit',(e) => {
     e.preventDefault();
     
     const location = search.value;
     messageOne.textContent = `Searching weather info of ${location}`;
-    messageTwo.textContent = 'Loading...'
+    messageTwo.textContent = 'Loading...';
+    messageThree.textContent = '';
+    image.style.display = 'none';
 
     fetch(`/weather?address=${location}`).then((response) => {
        response.json().then((data) => {
             if (data.error){
                 messageOne.textContent = data.error;
                 messageTwo.textContent = '';
+                messageThree.textContent = '';
             }  else {
-                messageOne.textContent = data.location;
-                messageTwo.textContent = data.forecast;
+                messageOne.textContent = `It is ${data.forecast.weather_descriptions} in ${location}`;
+                messageTwo.textContent = `it's currently ${data.forecast.temperature} degree and feels like ${data.forecast.feelslike} degree outside.`;
+                messageThree.textContent = data.location;
+                image.style.display = 'block';
+                image.src = data.forecast.weather_icons;
             }
        });
     });
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,14 +4,20 @@ const forecast = (latitude, longitude, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=f491d414cae5219cf34c65990625245a&query=${latitude},${longitude}&units=m`;
 
     request({url, json: true}, (error, response, body) => {
-        const {temperature, feelslike} = body.current;
+        const {temperature, feelslike, weather_icons, weather_descriptions} = body.current;
 
         if (error){
             callback('Unable to connect to service');
         } else if (body.error){
             callback('Unable to find the location');
         } else{
-            const dataReturn = `it's currently ${temperature} degree, it feels like ${feelslike} degree outside.`
+            // const dataReturn = `it's currently ${temperature} degree, it feels like ${feelslike} degree outside.`
+            const dataReturn = {
+                temperature,
+                feelslike,
+                weather_icons,
+                weather_descriptions
+            }
             callback(undefined, dataReturn);
         } 
     });
